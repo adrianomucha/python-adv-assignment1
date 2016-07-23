14af2de68f99f9aff1aea5a9863de3a9b2b31540
Adrian <adrianomucha@gmail.com>
2016-07-23 18:45:24 +0000

    assignment 1

---
+++ assignment1.py
@@ -0,0 +1,18 @@
+'''Extend the python dict class to enable accessing item by attribute.
+    exapmle:
+        your_dict = YourDict()
+        your dict['a'] = 1
+        print your_dict.a
+        your_dict.b = 2
+        print your_dict ['b']
+
+        '''
+
+class YourDict(dict):
+
+    def __setattr__(self,attr,value):
+        self[attr] = value
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
